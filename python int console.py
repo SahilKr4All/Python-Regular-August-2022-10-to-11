edea4b8e724af431a693676e685fa87a525b79a7
Sahil Kumar <101191455+SahilKr4All@users.noreply.github.com>
2022-08-12 17:26:01 +0530

    Add files via upload

---
+++ python int console.py
@@ -0,0 +1,61 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 12
+>>> y = 4
+>>> x+y
+16
+>>> x-y
+8
+>>> x*y
+48
+>>> x/y
+3.0
+>>> #power - **
+>>> 2**3
+8
+>>> 5**3
+125
+>>> #floor Divison -//
+>>> 22/7
+3.142857142857143
+>>> 22//7
+3
+>>> import math
+>>> math.pow(2,4)
+16.0
+>>> math.remainder(22,7)
+1.0
+>>> 22%7
+1
+>>> math.sqrt(100)
+10.0
+>>> math.sqrt(3)
+1.7320508075688772
+>>> math.sqrt(2)
+1.4142135623730951
+>>> math.gcd(900,81000)
+900
+>>> math.floor(345.5654)
+345
+>>> math.ceil(345.456)
+346
+>>> math.fabs(12)
+12.0
+>>> math.fabs(-12)
+12.0
+>>> math.comb(3,2)
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    math.comb(3,2)
+AttributeError: module 'math' has no attribute 'comb'
+>>> math.factorial(5)
+120
+>>> 5*4*3*2*1
+120
+>>> math.tau
+6.283185307179586
+>>> math.e
+2.718281828459045
+>>> math.pi
+3.141592653589793
+>>> 
